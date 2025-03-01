a16369523e0dbf8d41b8d8d514aa7e8a107b1fce
Code-MatrixDroid <the_danger_2@outlook.com>
2025-03-01 19:01:19 +0530

    fast-dl server setup
    Added files and index.html for fast-dl server and website

---
+++ fastdl/mods/rotu-r-alpha/startmod.bat
@@ -0,0 +1,2 @@
+cd ..\..\
+cod4x18_dedrun.exe +set fs_game mods/rotu-r-alpha +set net_ip 2405:201:a000:d2bc:fd0b:7fe5:e34f:b73 +set dedicated 2 +set ui_maxclients 64 +set sv_maxclients 64 +set sv_punkbuster 0 +exec general.cfg
